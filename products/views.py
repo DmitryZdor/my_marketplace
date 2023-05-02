2a6cfc17da8bf9ce74583855baa0b1995a5519ee
Дмитрий Здор <zdors@yandex.ru>
2023-05-02 16:54:16 +0700

    User_profile_registration_view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,4 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from products.models import Product, ProductCategory, ShoppingCart
 from users.models import User
 from django.contrib.auth.decorators import login_required
--- users/views.py
+++ users/views.py
@@ -4,7 +4,8 @@ from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth, messages
 from django.urls import reverse
 from products.models import ShoppingCart
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 
 
 def login(request):
@@ -23,36 +24,70 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Отлично! Регистрация прошла успешно!')
-            return HttpResponseRedirect(reverse('users:login'))
-    else:
-        form = UserRegistrationForm()
-    context = {'title': 'Регистрация', 'form': form}
-    return render(request, 'users/registration.html', context)
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title'] = 'TOPS_CROPS Регистрация'
+        return context
 
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user,
-                               data=request.POST,
-                               files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-    else:
-        form = UserProfileForm(instance=request.user)
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
 
-    context = {'title': 'Профиль',
-               'shopping_carts': ShoppingCart.objects.filter(
-                   user=request.user),
-               'form': form}
-    return render(request, 'users/profile.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = 'TOPS_CROPS Профиль'
+        context['shopping_carts'] = ShoppingCart.objects.filter(
+            user=self.object)
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Отлично! Регистрация прошла успешно!')
+#             return HttpResponseRedirect(reverse('users:login'))
+#     else:
+#         form = UserRegistrationForm()
+#     context = {'title': 'Регистрация', 'form': form}
+#     return render(request, 'users/registration.html', context)
+
+
+#
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         form = UserProfileForm(instance=request.user,
+#                                data=request.POST,
+#                                files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('users:profile'))
+#     else:
+#         form = UserProfileForm(instance=request.user)
+#
+#     context = {'title': 'Профиль',
+#                'shopping_carts': ShoppingCart.objects.filter(
+#                    user=request.user),
+#                'form': form}
+#     return render(request, 'users/profile.html', context)
 
 
 def logout(request):
